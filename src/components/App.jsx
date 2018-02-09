9995a83a03e60b4554ad88c9dc2a577ffb9ed38a
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-02-09 13:43:02 -0800

    Create header component render in app component. Import fonts using style jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import KegList from './KegList';
-
+import Header from './Header';
 import { Switch, Route } from 'react-router-dom';
 
 function App(){
   var appStyle = {
+
     margin: '0px',
     backgroundColor: '#FFFAFA',
     height: '100vh',
@@ -16,6 +17,15 @@ function App(){
   };
   return (
     <div style={appStyle}>
+      <style global jsx>{`
+        @import url('https://fonts.googleapis.com/css?family=Fredericka+the+Great|Josefin+Slab:400,700i');
+
+        body{
+          background-color: blue;
+        }
+      `}</style>
+
+      <Header/>
       <h1>App works</h1>
 
       <Switch>
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+function Header() {
+  return(
+    <div>
+      <h1>TAP ROOM</h1>
+    </div>
+  );
+}
+
+export default Header;
