212460c742178214245cbe3a53ee02eed2ccaffe
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-02-16 13:22:07 -0800

    Assign refs to inputs in form and define these variables as null to start.

--- src/components/NewKegForm.jsx
+++ src/components/NewKegForm.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function NewKegForm(props){
-  var formStyle = {
+  const formStyle = {
     display: 'flex',
     flexFlow: 'column nowrap',
     width: '500px',
-    height: '200px',
+    height: '300px',
     justifyContent: 'space-between',
     alignItems: 'center'
   };
+  let _name = null;
+  let _brewer = null;
+  let _description = null;
+  let _abv = null;
+  let _price = null;
+  let _remaining = null;
 
   return (
     <div >
@@ -17,14 +23,34 @@ function NewKegForm(props){
         <input
           type='text'
           id='name'
-          placeholder='Keg Name'/>
+          placeholder='Keg Name'
+          ref={(input) => {_name = input;}}/>
         <input
           type='text'
           id='brewer'
-          placeholder='Brewery'/>
+          placeholder='Brewery'
+          ref={(input) => {_brewer = input;}}/>
         <textarea
           id='Description'
-          placeholder='ex: Sparkling Wine with Grapefruit'/>
+          placeholder='ex: Sparkling Wine with Grapefruit'
+          ref={(input) => {_description = input;}}/>
+        <div className="smaller">
+          <input
+            type='text'
+            id='abv'
+            placeholder='ABV'
+            ref={(input) => {_abv = input;}}/>
+          <input
+            type='text'
+            id='price'
+            placeholder='$'
+            ref={(input) => {_price = input;}}/>
+          <input
+            type='text'
+            id='remaining'
+            placeholder='#'
+            ref={(input) => {_remaining = input;}}/>
+        </div>
         <button type='submit'>Add</button>
       </form>
 
@@ -41,12 +67,16 @@ function NewKegForm(props){
           }
           input, textarea{
             font-size: 1.25em;
+            text-align: center;
+            color: var(--darkblue);
             width: 80%;
             border-radius: 15px;
             border: 3px solid var(--blueblack);
             background-color: var(--light-peach);
           }
-
+          input::placeholder, textarea::placeholder{
+            color: var(--periwink);
+          }
           button {
             font-size: 1.25em;
             padding: 5px;
@@ -54,6 +84,17 @@ function NewKegForm(props){
             width: 90px;
             border: 3px solid var(--blueblack);
             border-radius: 15px;
+          }
+
+          .smaller {
+            width: 80%;
+            display: flex;
+            justify-content: space-around;
+          }
+
+          .smaller input{
+            width: 24%;
+            text-align: center;
           }
           `}</style>
     </div>
